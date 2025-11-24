c8722f93f0fc632bec17948a87b13e8507dbd233
Tristan Ross <tristan.ross@determinate.systems>
2025-11-23 16:16:25 -0800

    fix(river_adl): add test

--- packages/river_adl/test/river_adl_test.dart
+++ packages/river_adl/test/river_adl_test.dart
@@ -1,4 +1,34 @@
+import 'package:riscv/riscv.dart';
 import 'package:river_adl/river_adl.dart';
 import 'package:test/test.dart';
 
-void main() {}
+class MyModule extends Module {
+  DataField get c => output('c');
+
+  MyModule(DataField a, DataField b) : super() {
+    a = addInput('a', a);
+    b = addInput('b', b);
+
+    addOutput('c', type: a.type, source: DataLocation.register);
+
+    c.bind(a + b);
+  }
+}
+
+void main() {
+  group('MyModule', () {
+    final myModule = MyModule(
+      DataField.from(1, name: 'a'),
+      DataField.from(2, name: 'b'),
+    );
+
+    setUp(myModule.build);
+
+    test('Generated Assembly', () {
+      expect("""addi x4, x0, 1
+addi x5, x0, 2
+add x6, x5, x4
+""", myModule.generateAssembly());
+    });
+  });
+}
