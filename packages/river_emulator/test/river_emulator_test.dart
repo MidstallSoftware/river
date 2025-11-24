6819a85d8c5c22a0a70dd337a844219e590916a8
Tristan Ross <tristan.ross@determinate.systems>
2025-11-23 16:10:02 -0800

    fix(river_emulator): tests

--- packages/river_emulator/test/river_emulator_test.dart
+++ packages/river_emulator/test/river_emulator_test.dart
@@ -1,4 +1,18 @@
+import 'package:river/river.dart';
 import 'package:river_emulator/river_emulator.dart';
 import 'package:test/test.dart';
 
-void main() {}
+void main() {
+  group('Stream V1 - iCESugar', () {
+    test('Creation', () {
+      final socConfig = RiverSoCChoice.stream_v1.configure({
+        'platform': 'icesugar',
+      })!;
+
+      final soc = RiverSoCEmulator(socConfig);
+
+      expect(soc.devices.length, 8);
+      expect(soc.cores.length, 1);
+    });
+  });
+}
