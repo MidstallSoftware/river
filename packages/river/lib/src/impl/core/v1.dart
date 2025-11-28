e9b8fc722c7bcf784f31178a3421a10c0d1ad781
Tristan Ross <tristan.ross@determinate.systems>
2025-11-28 11:54:08 -0800

    feat(river): include rc1.s config

--- packages/river/lib/src/impl/core/v1.dart
+++ packages/river/lib/src/impl/core/v1.dart
@@ -47,4 +47,30 @@ class RiverCoreV1 extends RiverCore {
            rv32i,
          ],
        );
+
+  /// RC1.s - River Core V1 small
+  ///
+  /// A RV64IMAC core using the River design.
+  const RiverCoreV1.small({
+    super.vendorId = 0,
+    super.archId = 0,
+    super.hartId = 0,
+    super.resetVector = 0,
+    required super.mmu,
+    required super.interrupts,
+    required super.clock,
+    super.l1cache,
+  }) : super(
+         mxlen: Mxlen.mxlen_64,
+         extensions: const [
+           rvc,
+           rv32Zicsr,
+           rv32BasePrivilege,
+           rv32M,
+           rv64M,
+           rv32Atomics,
+           rv64Atomics,
+           rv32i,
+         ],
+       );
 }
